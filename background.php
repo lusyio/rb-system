c481d2322db8d9316cbef7fe6d3ded573e359660
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-06-22 18:08:24 +0300

    правка базы весысофт

--- background.php
+++ background.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
  	ini_set('error_reporting', E_ALL);
  	ini_set('display_errors', 1);
- 	ini_set('display_startup_errors', 1);	
-	include 'bdcon.php'; 
-	
+ 	ini_set('display_startup_errors', 1);
+	include 'bdcon.php';
+
 	$counts = DBOnce2('count(*) as kolvo','weighing','');
-	
+
 	// Выставление оценок для смен
 			$month = date("Y-m-01");
 			$now = date("Y-m-d");
@@ -13,105 +13,105 @@
 			$sql = $pdo->prepare($sql);
 			$sql->execute();
 			$sql = $sql->fetchAll(PDO::FETCH_BOTH);
-			
-			foreach ($sql as $result) {	
-				
+
+			foreach ($sql as $result) {
+
 				$sumsmena = $result['marten'] + $result['lpk'] + $result['don'];
-					   
+
 /*
-					    if ($result['repair'] == 'yes') {  $repball = 0;  } 
-					    if ($result['repair'] == 'no') {   $repball = 1;  } 
+					    if ($result['repair'] == 'yes') {  $repball = 0;  }
+					    if ($result['repair'] == 'no') {   $repball = 1;  }
 					    if ($result['hozwork'] == 'yes') {  $repball = 1; }
 */
 					    if ($result['hozwork'] == 'no') {   }
 					    if ($result['prostoy'] <= '01:00:00') {$prostoyball = 1;} else { $prostoyball = 0;  }
-					    
+
 					    $plansmena = $result['plansmena'];
-					    
+
 					    if ($sumsmena >= $plansmena) {
 						    $planball = 1;
 						    $updatemeta = 'UPDATE brigada SET plandone = "1" WHERE id = "'.$result['id'].'"';
 						    $updatemeta = $pdo->prepare($updatemeta);
 							$updatemeta->execute();
-						    
-						    
+
+
 					    } else {
 						    $planball = 0;
 						    $updatemeta = 'UPDATE brigada SET plandone = "0" WHERE id = "'.$result['id'].'"';
 						    $updatemeta = $pdo->prepare($updatemeta);
 							$updatemeta->execute();
 					    }
-					    
+
 					    if ($plansmena == 0) {
 						    $updatemeta = 'UPDATE brigada SET plandone = "1" WHERE id = "'.$result['id'].'"';
 						    $updatemeta = $pdo->prepare($updatemeta);
 							$updatemeta->execute();
-					    } 
+					    }
 					    $repball = 1;
 					    $meta = $planball + $repball + $prostoyball + 2;
-					    
+
 					    if ( $sumsmena == 0 and $result['updatework'] == 'no') { $meta = $repball;}
 					    if ( $result['updatework'] == 'yes' or $plansmena == 0 ) { $meta = 5; }
 					     if ($result['updatework'] == '') {  }
-					    
+
 						$updatemeta = 'UPDATE brigada SET meta = "'.$meta.'" WHERE id = "'.$result['id'].'"';
 						$updatemeta = $pdo->prepare($updatemeta);
 						$updatemeta->execute();
-					    
+
 			}
-	
-	
+
+
 	if(!empty($counts)) {
-			
-			
-			
+
+
+
 			// Обновление побочки смен
 			$sql = "SELECT datestart, timestart, brigadir, dateend, timeend, id FROM brigada WHERE datestart between '".$month."' and '".$now."' ORDER BY id DESC";
 		    $sql = $pdo->prepare($sql);
 			$sql->execute();
 			$sql = $sql->fetchAll(PDO::FETCH_BOTH);
-			
-			foreach ($sql as $result) {	
-			
+
+			foreach ($sql as $result) {
+
 			$datacreateresult = "TYP_EVENT='Покупка (приход от поставщика)' and DATETIME_CREATE between '".$result['datestart']." ".$result['timestart']."' and '".$result['dateend']." ".$result['timeend']."'";
-			
+
 			$martensmena = okr((DBOnce2("SUM(NETTO)","weighing","GRUZ_NAME='Карта выборки №1 МАРТЕН' and ".$datacreateresult))/1000);
 			$lpksmena = okr((DBOnce2("SUM(NETTO)","weighing","GRUZ_NAME='Карта выборки №2 ЛПК' and ".$datacreateresult))/1000);
 			$donsmena = okr((DBOnce2("SUM(NETTO)","weighing","GRUZ_NAME='Карта выборки №3 ДОНЫШКИ' and ".$datacreateresult))/1000);
 			$oplmat = okr((DBOnce2("SUM(NETTO)","weighing","GRUZ_NAME='Оплаченный материал' and ".$datacreateresult))/1000);
-			
-			
+
+
 			$updatesmena = 'UPDATE brigada SET marten = "'.$martensmena.'" WHERE id = "'.$result['id'].'"';
 			$updatesmena = $pdo->prepare($updatesmena);
 			$updatesmena->execute();
-			
+
 			$updatesmena = 'UPDATE brigada SET lpk = "'.$lpksmena.'" WHERE id = "'.$result['id'].'"';
 			$updatesmena = $pdo->prepare($updatesmena);
 			$updatesmena->execute();
-			
+
 			$updatesmena = 'UPDATE brigada SET don = "'.$donsmena.'" WHERE id = "'.$result['id'].'"';
 			$updatesmena = $pdo->prepare($updatesmena);
 			$updatesmena->execute();
-			
+
 			$updatesmena = 'UPDATE brigada SET oplmat = "'.$oplmat.'" WHERE id = "'.$result['id'].'"';
 			$updatesmena = $pdo->prepare($updatesmena);
 			$updatesmena->execute();
-			
-			
+
+
 	//		echo $martensmena. ' - ' . $lpksmena . ' - ' . $donsmena . ' - ' .$oplmat . '<br>';
 	}
-	
+
 	echo '<hr>';
-	
+
 	// мартен лпк и донышки за месяц
-	
+
 	$datemonth = "DATETIME_CREATE between '".$month." 00:00:00' and '".$now." 23:59:59'";
 
 	$marten = okr((DBOnce2("SUM(NETTO)","weighing","GRUZ_NAME='Карта выборки №1 МАРТЕН' and TYP_EVENT='Покупка (приход от поставщика)' and ".$datemonth))/1000);
 	$lpk = okr((DBOnce2("SUM(NETTO)","weighing","GRUZ_NAME='Карта выборки №2 ЛПК' and TYP_EVENT='Покупка (приход от поставщика)' and ".$datemonth))/1000);
 	$don = okr((DBOnce2("SUM(NETTO)","weighing","GRUZ_NAME='Карта выборки №3 ДОНЫШКИ' and TYP_EVENT='Покупка (приход от поставщика)' and ".$datemonth))/1000);
 	$oplmat = okr((DBOnce2("SUM(NETTO)","weighing","GRUZ_NAME='Оплаченный материал' and TYP_EVENT='Покупка (приход от поставщика)' and ".$datemonth))/1000);
-	
+
 	$updatemonth = 'UPDATE zenno SET value = "'.$marten.'" WHERE id = "10"';
 	$updatemonth = $pdo->prepare($updatemonth);
 	$updatemonth->execute();
@@ -127,22 +127,22 @@
 	$updatemonth = 'UPDATE zenno SET value = "'.$oplmat.'" WHERE id = "14"';
 	$updatemonth = $pdo->prepare($updatemonth);
 	$updatemonth->execute();
-				 
-				 
+
+
 	$allpoboch = $marten + $lpk + $don + $oplmat;
-	
+
 	echo 'За месяц: '.$allpoboch . '<br>';
 	//План смены
 	} else { }
-	
-	
-	
-	$nowtime = date("G"); 
+
+
+
+	$nowtime = date("G");
 	$yesterday = Date("Y-m-d",strtotime("-1 day"));
 	// Сколько взяли побочки за месяц
-	$zennopobo = DBOnce('value','zenno','id=1'); 
+	$zennopobo = DBOnce('value','zenno','id=1');
 	// План побочки на месяц
-	$planpoboch = DBOnce('value','zenno','id=7');  
+	$planpoboch = DBOnce('value','zenno','id=7');
 	$day = date('d');
 	$daysm = date('t');
 	$dayost2 = $daysm - $day;
@@ -153,11 +153,11 @@
 	}
 	if ($dayost2 == 1) {
 		$dayost = 1;
-	} 	
+	}
 	if ($dayost2 > 1) {
 		$dayost = $daysm - $day;
 	}
-	
+
 	if ($nowtime >=7 and $nowtime <19) {
 		$datasmen = $now;
 		$daynight = 'day';
@@ -169,7 +169,7 @@
 			$minus = 0;
 		}
 		$prosplan = (($planpoboch - $zennopobo)/($dayost*$kolsmen-$minus))*$kof;
-		
+
 		if ($prosplan < 0) {
 			$prosplan = 0;
 		}
@@ -179,31 +179,31 @@
 		$prosplan2 = number_format($prosplan,0,'',' ');
 		$pobochost = $planpoboch - $zennopobo;
 		if ($pobochost == $prosplan or $pobochost < 1000) {
-			
+
 			$sql = "SELECT marten, lpk, don, oplmat FROM brigada WHERE datestart = '".$datasmen."' and daynight = '".$daynight."'";
 			$sql = $pdo->prepare($sql);
 			$sql->execute();
 			$sql = $sql->fetchAll(PDO::FETCH_BOTH);
-			
+
 			foreach ($sql as $result2) {
 				$sumsmenanow = $result2['marten'] + $result2['lpk'] + $result2['don'] + $result2['oplmat'];
 			}
 			$prosplan = $prosplan + $sumsmenanow;
 		} else {
-			
+
 		}
 		if ($pobochost <= 0) {
 			$prosplan = 0;
 		}
 	//	if ($nowtime >=7 and $nowtime <12) {
-			 
+
 		$plansmena = 'UPDATE brigada SET plansmena = "'.$prosplan.'" WHERE datestart = "'.$datasmen.'" and daynight = "'.$daynight.'"';
 		$plansmena = $pdo->prepare($plansmena);
 		$plansmena->execute();
-			 
+
 	//	}
 	} else {
-		
+
 	}
 	if ($nowtime >=19 and $nowtime <24) {
 		$datasmen = $now;
@@ -223,13 +223,13 @@
 				    $sql = $pdo->prepare($sql);
 			$sql->execute();
 			$sql = $sql->fetchAll(PDO::FETCH_BOTH);
-			
+
 			foreach ($sql as $result2) {
 			$sumsmenanow = $result2['marten'] + $result2['lpk'] + $result2['don'] + $result2['oplmat'];
 		}
 			$prosplan = $prosplan + $sumsmenanow;
 		} else {
-			
+
 		}
 		if ($pobochost <= 0) {
 			$prosplan = 0;
@@ -238,65 +238,65 @@
 		$plansmena = $pdo->prepare($plansmena);
 		$plansmena->execute();
 	} else {
-		
+
 	}
 
 	if(!empty($counts)) {
 	//Обновление значения побочки за месяц
-	
+
 	$pobochall = 'UPDATE zenno SET value = "'.$allpoboch.'" WHERE id = "1"';
 	$pobochall = $pdo->prepare($pobochall);
-	$pobochall->execute();	
-	
+	$pobochall->execute();
+
 	// сколько всего щебня за месяц отгрузили
 	$vyborka = '(GRUZ_NAME = "Щебень 0-20 мм." OR GRUZ_NAME = "Щебень 20-40 мм." OR GRUZ_NAME = "Щебень 20-70 мм.") AND TYP_EVENT="Реализация (отгрузка покупателю)"';
 	$allshebsum = okr((DBOnce2('SUM(NETTO)','weighing',$vyborka.' and '.$datemonth))/1000);
-	
+
 	echo 'Щебень - '.$allshebsum;
 	$shebenreal = 'UPDATE zenno SET value = "'.$allshebsum.'" WHERE id = "13"';
 	$shebenreal = $pdo->prepare($shebenreal);
-	$shebenreal->execute();	
-	
+	$shebenreal->execute();
+
 	echo '<hr>';
-	
+
 	$vagon = DBOnce2("count(*) as kolvo","weighing","VESYNAME='Весы 3' and TYP_EVENT='Реализация (отгрузка покупателю)' and ".$datemonth);
 	echo 'В '.$vagon.'<br>';
-	
+
 	$vagonupdate = 'UPDATE zenno SET dop = "'.$vagon.'" WHERE id = "2"';
 	$vagonupdate = $pdo->prepare($vagonupdate);
-	$vagonupdate->execute();	
-	
+	$vagonupdate->execute();
+
 	$cars = DBOnce2("count(*)","weighing","VESYNAME='Весы 1' and TYP_EVENT='Реализация (отгрузка покупателю)' and ".$datemonth);
-	
+
 	echo 'М '.$cars;
 	$carsupdate = 'UPDATE zenno SET dop = "'.$cars.'" WHERE id = "3"';
 	$carsupdate = $pdo->prepare($carsupdate);
-	$carsupdate->execute();	
-	
+	$carsupdate->execute();
+
 	$deleteerror = "DELETE FROM `weighing` WHERE `TYP_EVENT` = 'ОШИБКА взвешивания' and ".$datemonth;
 	$deleteerror = $pdoves->prepare($deleteerror);
-	$deleteerror->execute();		 
-	} 
-	
+	$deleteerror->execute();
+	}
+
 	echo '<hr>';
-	
+
 	$tech = DB('*','tech_tech','');
-	
+
 	foreach ($tech as $n) {
-		
+
 		$tech = $n['id'];
 		// проверям есть ли актуальное ТО
 		$to = DBOnce('motchas','tech_work','status="inwork" and tech='.$n['id']);
-		
-		// смотрим последний моточасы для машины			
+
+		// смотрим последний моточасы для машины
 		$mch = DBOnce('motchas','tech_norm','tech='.$n['id'].' order by datetime DESC');
-		
+
 		$mch2 = DBOnce('motchas','tech_work','tech='.$n['id'].' and status="done" order by datetime DESC');
-		
+
 		$interval = 500;
 		$otschet = 0;
 		$predel = 400;
-		
+
 		if ($tech == 41 or $tech == 42 ) {
 			$interval = 500;
 		}
@@ -309,26 +309,26 @@
 
         if ($tech == 41) {
 			$otschet = 8700;
-		}	
+		}
 		if ($tech == 42) {
 			$otschet = 5500;
-		}		
+		}
 		if ($tech == 46) {
 			$otschet = 25267;
 		}
-		
+
 		$new = (okr(($mch - $otschet) / $interval)*$interval) - ($mch - $otschet);
 		if (!empty($mch2)) {
 			$razn = $mch - $mch2;
 		} else {
 			$razn = 1;
 		}
-		
+
 		echo '<p>' . DBOnce('name','tech_tech','id='.$tech) . ': ' . $mch . ' м/ч: ' . $new . '. Разница = '.$razn.'</p>';
-		
-		
+
+
 		if ($new <= 100 and $new > 0) {
-			
+
 			if(empty($to)) {
 				echo 'Хочу добавить ТО ';
 				if ($razn == 1 or $razn >= $predel) {
@@ -343,14 +343,27 @@
 					echo 'Но не добавляю :-(';
 				}
 			}
-			
-			
-			
-		} 
+
+
+
+		}
 
 	}
-	
-	
+
+
+  // правка базы весысофт
+
+  $DOC = '';
+
+  if (!empty($DOC)) :
+
+    $vesyupdate = 'UPDATE weighing SET TARA = "17500" WHERE DOC='.$DOC;
+  	$vesyupdate = $pdoves->prepare($vesyupdate);
+  	$vesyupdate->execute();
+
+  endif;
+
+
 	// Обновляем время
 	$arr = [
 				  'января',
@@ -374,8 +387,8 @@
 	if(!empty($counts)) {
 	$zennoupdate = 'UPDATE zenno SET dop = "'.$dateupdate.'" WHERE id = "9"';
 	$zennoupdate = $pdo->prepare($zennoupdate);
-	$zennoupdate->execute();	
+	$zennoupdate->execute();
 	} else {
-		
+
 	}
 ?>
