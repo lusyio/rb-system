26b127109e038639e8f8af650cb9c85e17b2aae4
lusyio <mr-kelevras@yandex.ru>
2020-12-11 13:56:15 +0300

    Update cron-report.php

--- cron-report.php
+++ cron-report.php
@@ -4,7 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 include 'bdcon.php';
 
-$emails = ['mr-kelevras@yandex.ru','dimkaFPS@gmail.com','fso20061@yandex.ru'];
+$emails = ['mr-kelevras@yandex.ru'];
+//$emails = ['mr-kelevras@yandex.ru','dimkaFPS@gmail.com','fso20061@yandex.ru'];
 require_once 'includes/phpmailer/RBMailer.php';
 $mail = new \PHPMailer\PHPMailer\RBMailer();
 try {
