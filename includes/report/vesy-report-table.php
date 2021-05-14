8494288c6954256fc6b635345aa2cd6af7814ffb
Dmitry RichBee <49393913+lusyio@users.noreply.github.com>
2021-05-14 13:49:05 +0300

    Update vesy-report-table.php

--- includes/report/vesy-report-table.php
+++ includes/report/vesy-report-table.php
@@ -6,66 +6,66 @@ echo	'<thead>
 				      if (empty($polychatel)) {
 					      echo '<th scope="col">Получатель</th>';
 				      } else {
-					      
+
 				      }
 				  echo '<th scope="col">Груз</th>';
-				  if ($polychatel == 'АО ВМЗ') {
+				  if ($polychatel == 'ВМЗ') {
 					      echo '<th scope="col">№ ТТН</th>';
 				      } else {
-					      
+
 				      }
 				  echo   '<th scope="col">Тара</th>
 				      <th scope="col">Нетто</th>';
-				      
+
 				echo '    </tr>
 				  </thead>
 				  <tbody>';
-				  
-				  
+
+
 				  $sql = $pdoves->prepare($sql);
 				  $sql->execute();
 				  $sql = $sql->fetchAll(PDO::FETCH_BOTH);
-						
+
 				  foreach ($sql as $result) {
-					  
-					$newDate2 = date("d.m.Y H:i", strtotime($result['DATETIME_CREATE']));	  
+
+					$newDate2 = date("d.m.Y H:i", strtotime($result['DATETIME_CREATE']));
 					$tara = $result['TARA']/1000;
 					$ves = $result['NETTO']/1000;
 					echo '<tr><td>'.$newDate2.'</td><td>'.$result['NOMER_TS'].'</td>';
 					if (empty($polychatel)) {
 					      echo '<td>'.$result['FIRMA_POL'].'</d>';
 				      } else {
-					      
+
 				      }
-					
+
 					echo '<td>'.$result['GRUZ_NAME'].'</td>';
-					if ($polychatel == 'АО ВМЗ') {
+					if ($polychatel == 'ВМЗ') {
 					      echo '<td>'.$result['DOC'].'</td>';
 				      } else {
-					      
+
 				      }
 					echo '<td>'.$tara.' т.</td><td>'.$ves.' т.</td>';
-					
-					
+
+
 				echo	'</tr>';
-					  
+
 				    }
-				    
+
 				  $all2 = $all/1000;
 				  echo '<tr><td> </td><td> </td>';
 				  if (empty($polychatel)) {
 					      echo '<td></d>';
 				      } else {
-					      
+
 				      }
 				  echo '<td></td>';
-				  if ($polychatel == 'АО ВМЗ') {
+				  if ($polychatel == 'ВМЗ') {
 					      echo '<td></td>';
 				      } else {
-					      
+
 				      }
 				  echo '<td><strong>Всего:</strong></td><td><strong>'.$all2.' т.</strong></td>';
-				    
+
 				  echo '</tr>
 			</tbody>
 		</table>';
